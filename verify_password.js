9155212165fc2edc2b9652c53fe1793b3825b86a
Abhijeet Singh <ak.abhijeet55@gmail.com>
2021-06-28 15:03:34 +0530

    Added logic to verify passsword

--- verify_password.js
+++ verify_password.js
@@ -1,7 +1,20 @@
 function verifyPassword(password) {
     // Perform operation
-    // 1. Check for null and throw 
-    if (password === '' || password.length <= 0 ) {
-        throw 'Password can not be null or empty'; 
+    // Check for null or empty 
+    if (password === null || password === '' || password.length <= 0 ) {
+        throw "Password can not be null or empty"; 
+    }
+    else if (password.length < 9 ) {
+        throw "Password should be larger than 8 characters";
+    }
+    else if (!/[A-Z]/.test(password)) {   // Check the string containe capital letter
+        throw "Password must contain a capital letter";
+    }  
+    else if (!/[a-z]/.test(password)) { // Check the string containe small letter
+        throw "Password must contain a small letter";
+    }
+
+    else if (!/[0-9]/.test(password)) { // Check the string contain a number
+        throw "Password must contain a number";
     }
 }
